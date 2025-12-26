661b89aac75779f9097aba7d8c5aff67d1566343
ThilinaRodrigo <thilinalakshan2001@gmail.com>
2025-12-26 22:49:21 +0530

    Refactor application configuration to YAML format, add gateway server routes, and update Java version to 21

--- gateway-server/src/main/java/com/jobportal/gateway_server/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/jobportal/gateway_server/GatewayServerApplication.java
@@ -2,6 +2,9 @@ package com.jobportal.gateway_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServerApplication {
@@ -10,4 +13,30 @@ public class GatewayServerApplication {
 		SpringApplication.run(GatewayServerApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator customRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+
+				.route(p -> p
+						.path("/jobportal/accounts/**")
+						.filters(f -> f.stripPrefix(1))
+						.uri("lb://EMPLOYEE-SERVICE"))
+
+				.route(p -> p
+						.path("/jobportal/cards/**")
+						.filters(f -> f.stripPrefix(1))
+						.uri("lb://EMPLOYER-SERVICE"))
+
+				.route(p -> p
+						.path("/jobportal/applications/**")
+						.filters(f -> f.stripPrefix(1))
+						.uri("lb://APPLICATION-SERVICE"))
+
+				.route(p -> p
+						.path("/jobportal/jobs/**")
+						.filters(f -> f.stripPrefix(1))
+						.uri("lb://JOB-SERVICE"))
+
+				.build();
+	}
 }
